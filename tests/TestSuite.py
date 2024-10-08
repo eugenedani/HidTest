331bda310a1e1b0bdef9f8c3ebc506865e8583a0
Evgeny Danilov <evgeny.m.danilov@gmail.com>
2024-10-08 16:01:11 +0200

    Add HashFree method testing to Suite

--- tests/TestSuite.py
+++ tests/TestSuite.py
@@ -1,6 +1,7 @@
 import unittest
 
 import tests.Directory
+import tests.Free
 import tests.InitializeTerminate
 import tests.ReadLine
 import tests.Status
@@ -11,10 +12,11 @@ def suite():
     loader = unittest.TestLoader()
     hash_suite = unittest.TestSuite()
     hash_suite.addTest(loader.loadTestsFromModule(tests.InitializeTerminate))
+    hash_suite.addTest(loader.loadTestsFromModule(tests.ReadLine))
     hash_suite.addTest(loader.loadTestsFromModule(tests.Directory))
     hash_suite.addTest(loader.loadTestsFromModule(tests.Status))
-    hash_suite.addTest(loader.loadTestsFromModule(tests.ReadLine))
     hash_suite.addTest(loader.loadTestsFromModule(tests.Stop))
+    hash_suite.addTest(loader.loadTestsFromModule(tests.Free))
     return hash_suite
 
 
