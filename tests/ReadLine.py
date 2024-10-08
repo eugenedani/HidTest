f2dd86f9c677f4ee84b500c16eea931ad41e5499
Evgeny Danilov <evgeny.m.danilov@gmail.com>
2024-10-08 15:31:30 +0200

    Commit read lines tests

--- tests/ReadLine.py
+++ tests/ReadLine.py
@@ -1,7 +1,9 @@
 import ctypes
+import os
 import time
 import unittest
 
+import CheckSum
 import Hash
 from Error import Error
 
@@ -20,23 +22,28 @@ class TestReadLine(unittest.TestCase):
 
     def testReadLines(self):
         identifier = ctypes.c_size_t()
-        b_path = "../libraries".encode("utf-8")
+        folder_name = "../libraries"
+        b_path = folder_name.encode("utf-8")
         run_status = ctypes.c_bool()
         hash_content = ctypes.c_char_p()
+        lines = []
         self.hash.HashInit()
         self.hash.HashDirectory(b_path, ctypes.byref(identifier))
         while self.hash.HashStatus(identifier.value, ctypes.byref(run_status)) == 0 and run_status.value:
             pass
         code: int = self.hash.HashReadNextLogLine(hash_content)
-        self.check_content(hash_content.value)
+        lines.append(hash_content.value)
+        self.hash.HashFree(hash_content)
+        time.sleep(0.1)
         while self.hash.HashReadNextLogLine(hash_content) == 0:
-            self.check_content(hash_content.value)
-            # self.hash.HashFree(hash_content)
+            lines.append(hash_content.value)
+            self.hash.HashFree(hash_content)
             time.sleep(0.1)
         self.hash.HashStop(identifier)
         self.assertEqual(Error.HASH_ERROR_OK.value, code, f"Error code should be {Error.HASH_ERROR_OK} but was {code}")
+        self.check_content(folder_name, lines, identifier)
 
-    def testLogEmpty(self):
+    def testLogEmptyFolder(self):
         identifier = ctypes.c_size_t()
         b_path = "../libraries/empty".encode("utf-8")
         run_status = ctypes.c_bool()
@@ -45,6 +52,21 @@ class TestReadLine(unittest.TestCase):
         self.hash.HashDirectory(b_path, ctypes.byref(identifier))
         while self.hash.HashStatus(identifier.value, ctypes.byref(run_status)) == 0 and run_status.value:
             pass
+        code: int = self.hash.HashReadNextLogLine(hash_content)
+        self.hash.HashStop(identifier)
+        self.assertEqual(Error.HASH_ERROR_LOG_EMPTY.value, code,
+                         f"Error code should be {Error.HASH_ERROR_LOG_EMPTY} but was {code}")
+
+    def testLogEmpty(self):
+        identifier = ctypes.c_size_t()
+        b_path = "../libraries/one".encode("utf-8")
+        run_status = ctypes.c_bool()
+        hash_content = ctypes.c_char_p()
+        self.hash.HashInit()
+        self.hash.HashDirectory(b_path, ctypes.byref(identifier))
+        while self.hash.HashStatus(identifier.value, ctypes.byref(run_status)) == 0 and run_status.value:
+            pass
+        self.hash.HashReadNextLogLine(hash_content)
         code: int = self.hash.HashReadNextLogLine(hash_content)
         self.hash.HashStop(identifier)
         self.assertEqual(Error.HASH_ERROR_LOG_EMPTY.value, code,
@@ -68,9 +90,30 @@ class TestReadLine(unittest.TestCase):
     #    code: int = self.hash.HashReadNextLogLine(ctypes.byref(hash_content))
     #    self.assertEqual(Error.HASH_ERROR_ARGUMENT_INVALID.value, code, f"Error code should be {Error.HASH_ERROR_ARGUMENT_INVALID} but was {code}")
 
-    def check_content(self, hash_content):
-        print(hash_content)
+    def check_content(self, folder_name, lines, lib_id):
+        files = []
+        folder = os.scandir(folder_name)
+        for entry in folder:
+            if entry.is_file():
+                files.append(entry)
 
+        self.assertEqual(len(lines), len(files),
+                         f"Hasl library found unexpected amount of files in {folder_name} folder")
+
+        for i in range(len(lines) - 1):
+            content: str = lines[i].decode("utf-8")
+            values = content.split(" ")
+            self.assertEqual(3, len(values), f"Line from Hash library has wrong format. File {files[i].name}")
+            self.assertTrue(values[0].isdigit(),
+                            f"Line from Hash library has wrong identifier: {values[0]} File {files[i].name} ")
+            self.assertEqual(lib_id.value, int(values[0]),
+                             f"Line from Hash library has wrong identifier. File {files[i].name}")
+            self.assertIn(files[i].name, values[1], "Line from Hash library has wrong file name")
+            md5 = CheckSum.CheckSum(files[i].path).md5()
+            # It is not clear if md5 checksum should have capital letters or not capital letters
+            # If it should have not capital letters then it is a bug because it has capital letters
+            self.assertEqual(md5, values[2].lower(),
+                             f"Line from Hash library has wrong MD5 checksum. File {files[i].name}")
 
 if __name__ == '__main__':
     unittest.main()  # run all tests
